cfa56b28b55bc882ba1da83a80d4d9557d927dca
Yayeeter <Phakin.k@mail.kmutt.ac.th>
2022-09-22 22:59:07 +0700

    merging

--- bloodgroup.c
+++ bloodgroup.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 void main()
- 	//chagne Name[50], G[5], answer[10] -> char Name[128], G[32], answer[32] -> g[101], answer[96]
-{ char char Name[128], G[101], answer[96];
+//chagne Name[128], G[32], answer[32] -> Name[123], G[456]
+{ char char Name[128], G[32], answer[32];
   int i;
   do
   {
